4a0a2d2c6e0086ab991d0ca530f6fa9dfabcf195
LuA1i <117823970+LuA1i@users.noreply.github.com>
2024-03-17 20:46:56 +0000

    progress day2

--- script.js
+++ script.js
@@ -2,12 +2,28 @@ const addButton = document.querySelector('#add_item')
 const emptyList = document.querySelector('#list_adder')
 const inputField = document.querySelector('#input_task')
 
-addButton.addEventListener('click', function () {
-  const inputfeildEmpty = inputField.value.trim()
+function checkboxCross(e) {
+  const checkbox = e.target
 
-  if (inputfeildEmpty !== '') {
+  const listItem = checkbox.parentNode
+
+  listItem.classList.toggle('completed', checkbox.checked)
+}
+
+addButton.addEventListener('click', function () {
+  const inputFieldValue = inputField.value.trim()
+
+  if (inputFieldValue !== '') {
+    const checkBox = document.createElement('input')
+    checkBox.type = 'checkbox'
+    checkBox.style.marginRight = '5px'
+
     const addlist = document.createElement('li')
-    addlist.innerText = inputField.value
+    addlist.appendChild(checkBox)
+    addlist.appendChild(document.createTextNode(inputFieldValue))
+
+    checkBox.addEventListener('click', checkboxCross)
+
     emptyList.appendChild(addlist)
     inputField.value = ''
   } else {
