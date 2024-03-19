647f6289de9834d332156861b811239a094053f0
LuA1i <117823970+LuA1i@users.noreply.github.com>
2024-03-19 13:42:53 +0000

    added button functionlities

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const addButton = document.querySelector('#add_item')
 const emptyList = document.querySelector('#list_adder')
 const inputField = document.querySelector('#input_task')
+const activeTaskbtn = document.querySelector('#see_active_tasks')
+const completedTaskbtn = document.querySelector('#see_completed_tasks')
+const allTaskbtn = document.querySelector('#see_all_tasks')
 
+// --------------------------------------------------------------Saving Tasks Locally------------------------------------------//
 const todos = JSON.parse(localStorage.getItem('todos')) || []
 
 function saveTodos() {
@@ -43,10 +47,110 @@ function loadTodos() {
 
     emptyList.appendChild(addlist)
   })
+} // --------------------------------------------------------------Showing Active Task--------------------------------------------//
+
+activeTaskbtn.addEventListener('click', showActiveTask)
+
+function showActiveTask() {
+  emptyList.innerHTML = ''
+
+  const activeTasks = todos.filter((todo) => !todo.completed)
+  activeTasks.forEach((checkList, index) => {
+    const checkBox = document.createElement('input')
+    const deleteButton = document.createElement('button')
+
+    deleteButton.innerHTML = 'Delete Task'
+
+    checkBox.type = 'checkbox'
+    checkBox.style.marginRight = '5px'
+    deleteButton.style.marginLeft = '65%'
+
+    const addlist = document.createElement('li')
+    addlist.setAttribute('data-index', index)
+    addlist.classList.add('list-item')
+
+    addlist.appendChild(checkBox)
+    addlist.appendChild(document.createTextNode(checkList.name))
+    addlist.appendChild(deleteButton)
+
+    checkBox.checked = checkList.completed
+    addlist.classList.toggle('completed', checkList.completed)
+
+    checkBox.addEventListener('click', checkboxCross)
+
+    emptyList.appendChild(addlist)
+  })
+}
+// --------------------------------------------------------------Showing Completed Task------------------------------------------//
+completedTaskbtn.addEventListener('click', showCompletedTask)
+
+function showCompletedTask() {
+  emptyList.innerHTML = ''
+  const completedTask = todos.filter((todo) => todo.completed)
+
+  completedTask.forEach((checkList, index) => {
+    const checkBox = document.createElement('input')
+    const deleteButton = document.createElement('button')
+
+    deleteButton.innerHTML = 'Delete Task'
+
+    checkBox.type = 'checkbox'
+    checkBox.style.marginRight = '5px'
+    deleteButton.style.marginLeft = '65%'
+
+    const addlist = document.createElement('li')
+    addlist.setAttribute('data-index', index)
+    addlist.classList.add('list-item')
+
+    addlist.appendChild(checkBox)
+    addlist.appendChild(document.createTextNode(checkList.name))
+    addlist.appendChild(deleteButton)
+
+    checkBox.checked = checkList.completed
+    addlist.classList.toggle('completed', checkList.completed)
+
+    checkBox.addEventListener('click', checkboxCross)
+
+    emptyList.appendChild(addlist)
+  })
 }
 
 loadTodos()
+// --------------------------------------------------------------Showing Completed Task------------------------------------------//
 
+allTaskbtn.addEventListener('click', showAllTasks)
+
+function showAllTasks() {
+  emptyList.innerHTML = ''
+
+  todos.forEach((checkList, index) => {
+    const checkBox = document.createElement('input')
+    const deleteButton = document.createElement('button')
+
+    deleteButton.innerHTML = 'Delete Task'
+
+    checkBox.type = 'checkbox'
+    checkBox.style.marginRight = '5px'
+    deleteButton.style.marginLeft = '65%'
+
+    const addlist = document.createElement('li')
+    addlist.setAttribute('data-index', index)
+    addlist.classList.add('list-item')
+
+    addlist.appendChild(checkBox)
+    addlist.appendChild(document.createTextNode(checkList.name))
+    addlist.appendChild(deleteButton)
+
+    checkBox.checked = checkList.completed
+    addlist.classList.toggle('completed', checkList.completed)
+
+    checkBox.addEventListener('click', checkboxCross)
+
+    emptyList.appendChild(addlist)
+  })
+}
+
+// --------------------------------------------------------------Add Tasks Button------------------------------------------//
 addButton.addEventListener('click', function () {
   const inputFieldValue = inputField.value.trim()
 
